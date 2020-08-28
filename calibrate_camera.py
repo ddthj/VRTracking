c9e39199b6a67d5f091908ba78edc73ec7d51c34
ddthj <ddthj@users.noreply.github.com>
2020-08-27 21:18:47 -0400

    cleaned

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -5,8 +5,8 @@ import numpy as np
 def calibration_process():
     print("No camera calibration detected. Calibration process started...")
     # Defining the dimensions of checkerboard
-    CHECKERBOARD = (6, 9)
-    CHECKER_SIZE = float(input("Size of Checkerboard Squares in mm: "))  # mm
+    checkerboard = (4, 5)
+    checker_size = float(input("Size of Checkerboard Squares in mm: "))  # mm
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
     # Creating vector to store vectors of 3D points for each checkerboard image
@@ -15,8 +15,8 @@ def calibration_process():
     imgpoints = []
 
     # Defining the world coordinates for 3D points
-    objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-    objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2) * CHECKER_SIZE
+    objp = np.zeros((1, checkerboard[0] * checkerboard[1], 3), np.float32)
+    objp[0, :, :2] = np.mgrid[0:checkerboard[0], 0:checkerboard[1]].T.reshape(-1, 2) * checker_size
 
     capture = cv2.VideoCapture(0)
     images_captured = 0
@@ -24,28 +24,27 @@ def calibration_process():
     while True:
         _, img = capture.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD)
+        ret, corners = cv2.findChessboardCorners(gray, checkerboard)
         if ret:
             objpoints.append(objp)
             # refining pixel coordinates for given 2d points.
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-
             imgpoints.append(corners2)
 
             # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+            img = cv2.drawChessboardCorners(img, checkerboard, corners2, ret)
             images_captured += 1
             print("Captured %s/20 images" % images_captured)
 
         cv2.imshow('img', img)
         if images_captured >= 20:
             ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-            np.savez("calibration.npz", name1=mtx, name2=dist, name3=rvecs, name4=tvecs)
+            np.savez("calibration.npz", name1=mtx, name2=dist)
             print("Calibration Complete!")
             cv2.destroyAllWindows()
-            return mtx, dist, rvecs, tvecs
+            return mtx, dist
 
-        if cv2.waitKey(10) & 0xFF == ord("q") or images_captured >= 20:
+        if cv2.waitKey(10) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
     print("Calibration Failed!")
@@ -56,8 +55,6 @@ def get_calibration():
         camera_data = np.load("calibration.npz")
         camera_matrix = camera_data["name1"]
         camera_dist = camera_data["name2"]
-        camera_rvecs = camera_data["name3"]
-        camera_tvecs = camera_data["name4"]
-        return camera_matrix, camera_dist, camera_rvecs, camera_tvecs
+        return camera_matrix, camera_dist
     except:
         return calibration_process()
--- calibrate_object.py
+++ calibrate_object.py
@@ -9,6 +9,7 @@ s = 13.25
 FIDUCIAL_CORNERS = np.float32([[-s, s, 0], [s, s, 0], [s, -s, 0], [-s, -s, 0]]).reshape(-1, 3)
 FIDUCIAL_CENTER = np.array([s, s, 0])
 
+
 class TrackedObject:
     def __init__(self, name):
         self.name = name
@@ -23,126 +24,100 @@ class Fiducial:
         self.translation = translation
 
 
-def rotation_matrix(pitch, yaw, roll):
-    cp = math.cos(pitch)
-    sp = math.sin(pitch)
-    cy = math.cos(yaw)
-    sy = math.sin(yaw)
-    cr = math.cos(roll)
-    sr = math.sin(roll)
-    return np.array([[cp * cy, cp * sy, sp],
-                     [cy * sp * sr - cr * sy, sy * sp * sr + cr * cy, -cp * sr],
-                     [-cr * cy * sp - sr * sy, -cr * sy * sp + sr * cy, cp * cr]])
-
-
 def calibrate(tracked: TrackedObject):
     print("Object '%s' not calibrated. Calibration will begin" % tracked.name)
-    # count = int(input("Please enter the number of fiducials on the object: "))
+    count = 99  # int(input("Please enter the number of fiducials on the object: "))
     tags, parems, fid_points = get_fiducials()
-    camera_matrix, camera_dist, camera_rvecs, camera_tvecs = get_calibration()
+    camera_matrix, camera_dist = get_calibration()
     cap = cv2.VideoCapture(0)
 
-    while True:
+    while len(tracked.fiducials) < count:
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # corners, ids, rejectedimgpoints
+        # corners, ids, debug
         corner_list, ids, _ = cv2.aruco.detectMarkers(gray, tags, parameters=parems)
-        found_link = False
-        # verify that we actually found fiducials
+        link = None
+        link_is_reference = False
         if ids is not None:
-            ids = [str(x[0]) for x in ids]
-            corner_list = [x[0] for x in corner_list]
-            # For each fiducial we can see
+            ids = list(ids.ravel())
             for i in range(len(ids)):
-                # Get a list of fiducial ids that have already been linked back to the reference
                 keys = tracked.fiducials.keys()
                 ident = ids[i]
-                corners = corner_list[i]
-                center = tuple(np.mean(corners, axis=0))
-                frame = cv2.putText(frame, ident, center, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), thickness=2)
+                corners = corner_list[i][0]
+
                 if tracked.reference is None:
                     # If our tracked object lacks a reference point, we make this one the reference
                     tracked.reference = ident
                     tracked.fiducials[ident] = Fiducial(ident, None, None)
+                    link = tracked.fiducials[ident]
+                    link_is_reference = True
                     print("selected %s as reference" % ident)
                 elif ident not in keys:
                     print("%s not in keys, will attempt to link" % ident)
-                    # If this fiducial hasn't been linked yet we get its orientation matrix
-                    ident_ret, ident_rot, ident_vec = cv2.solvePnP(fid_points, corners, camera_matrix, camera_dist,
-                                                                   cv2.SOLVEPNP_ITERATIVE)
-                    ident_vec = ident_vec.reshape(3)
-                    ident_matrix = rotation_matrix(*ident_rot)
+                    # To add a new fiducial we first need to identify an existing one, preferably the reference
                     if tracked.reference in ids:
                         print("reference is still visible, making direct link")
-                        # If we can see the reference, we get its orientation matrix
-                        ref_corners = corner_list[ids.index(tracked.reference)]
-                        ref_ret, ref_rot, ref_vec = cv2.solvePnP(fid_points, ref_corners, camera_matrix, camera_dist,
-                                                                 cv2.SOLVEPNP_ITERATIVE)
-                        ref_vec = ref_vec.reshape(3)
-                        ref_matrix = rotation_matrix(*ref_rot)
-                        found_link = True
-
-                    else:
+                        link = tracked.fiducials[tracked.reference]
+                        link_is_reference = True
+                    elif link is None:
                         print("reference isn't visible, searching for another linked fiducial")
-                        # We can't see the reference, so instead we must find another linked fiducial
                         for j in range(len(ids)):
                             if j != i and ids[j] in keys:
-                                linked = tracked.fiducials[ids[j]]
-                                print("selected %s to make indirect link" % linked.ident)
-                                linked_corners = corner_list[ids.index(linked.ident)]
-                                linked_ret, linked_rot, linked_vec = cv2.solvePnP(fid_points, linked_corners,
-                                                                                  camera_matrix,
-                                                                                  camera_dist,
-                                                                                  cv2.SOLVEPNP_ITERATIVE)
-                                linked_vec = linked_vec.reshape(3)
-                                linked_matrix = rotation_matrix(*linked_rot)
-                                # Once we have the linked fiducial's information we can solve for the reference fiducial
-                                ref_matrix = np.dot(linked_matrix, linked.rotation)
-                                ref_vec = np.dot(linked.translation, linked.rotation.T) + linked_vec
-                                found_link = True
+                                link = tracked.fiducials[ids[j]]
+                                print("selected %s to make indirect link" % ids[j])
                                 break
-                    if found_link:
-                        # Now that we have the reference pose we can link the new fiducial to it
-                        # We find the transformation to get from the current orientation to the reference's orientation
-                        ident_to_ref_matrix = np.dot(np.linalg.inv(ident_matrix), ref_matrix)
-                        relative = ref_vec - ident_vec
-                        local_distance = np.dot(relative, ident_matrix)
-                        tracked.fiducials[ident] = Fiducial(ident, ident_to_ref_matrix, local_distance)
 
-                        fid = tracked.fiducials[ident]
-                        print("successfully linked %s" % ident)
-                        print("ref location:")
-                        print(ref_vec)
-                        print("estimated ref location:")
-                        print(np.dot(fid.translation, fid.rotation.T) + ident_vec)
-                        print("ref orientation:")
-                        print(ref_matrix)
-                        print("estimated ref orientation:")
-                        print(np.dot(ident_matrix, fid.rotation))
+                    if link is not None:
+                        # Now that we have a link, we find the pose of the ident and link
+                        ident_ret, ident_rot, ident_vec = cv2.solvePnP(fid_points, corners, camera_matrix, camera_dist,
+                                                                       cv2.SOLVEPNP_ITERATIVE)
+                        ident_matrix = cv2.Rodrigues(ident_rot, np.identity(3), jacobian=0)[0]
+                        ident_loc = ident_vec.reshape(3) + FIDUCIAL_CENTER.dot(ident_matrix.T)
+
+                        link_corners = corner_list[ids.index(link.ident)][0]
+                        link_ret, link_rot, link_vec = cv2.solvePnP(fid_points, link_corners, camera_matrix,
+                                                                    camera_dist,
+                                                                    cv2.SOLVEPNP_ITERATIVE)
+                        link_matrix = cv2.Rodrigues(link_rot, np.identity(3), jacobian=0)[0]
+                        link_loc = link_vec.reshape(3) + FIDUCIAL_CENTER.dot(link_matrix.T)
+
+                        if link_is_reference:
+                            ref_matrix = link_matrix
+                            ref_loc = link_loc
+                        else:
+                            ref_matrix = link.rotation.dot(ident_matrix)
+                            ref_loc = ident_loc + ident_matrix.dot(link.translation)
+
+                        ident_to_ref_matrix = np.linalg.inv(ident_matrix).dot(ref_matrix)
+                        relative = ref_loc - ident_loc
+                        print("relative distance: ", relative)
+                        local_distance = relative.dot(ident_matrix)
+                        tracked.fiducials[ident] = Fiducial(ident, ident_to_ref_matrix, local_distance)
                     else:
                         print("couldn't find a linked fiducial!")
 
-                #Debug stuff
-                elif ident != tracked.reference and tracked.reference in ids:
-                    fid = tracked.fiducials[ident]
-                    ref_corners = corner_list[ids.index(tracked.reference)]
-                    ref_ret, ref_rot, ref_vec = cv2.solvePnP(fid_points, ref_corners, camera_matrix, camera_dist,
-                                                             cv2.SOLVEPNP_ITERATIVE)
-                    ref_vec = ref_vec.reshape(3)
-                    ref_matrix = rotation_matrix(*ref_rot)
-
-                    ident_ret, ident_rot, ident_vec = cv2.solvePnP(fid_points, corners, camera_matrix, camera_dist,
-                                                                   cv2.SOLVEPNP_ITERATIVE)
-                    ident_vec = ident_vec.reshape(3)
-                    ident_matrix = rotation_matrix(*ident_rot)
-
-                    print(ref_vec, np.dot(fid.translation, fid.rotation.T) + ident_vec)
+                # Debug stuff
                 elif ident != tracked.reference:
-                    fid = tracked.fiducials[ident]
+                    link = tracked.fiducials[ident]
+                    point = np.array([0, 0, 0], dtype=np.float32).reshape(3)
                     ident_ret, ident_rot, ident_vec = cv2.solvePnP(fid_points, corners, camera_matrix, camera_dist,
                                                                    cv2.SOLVEPNP_ITERATIVE)
-                    ident_vec = ident_vec.reshape(3)
-                    print(np.dot(fid.translation, fid.rotation.T) + ident_vec)
+                    ident_matrix = cv2.Rodrigues(ident_rot, np.identity(3), jacobian=0)[0]
+                    ident_loc = ident_vec.reshape(3) + FIDUCIAL_CENTER.dot(ident_matrix.T)
+
+                    projected, _ = cv2.projectPoints(point, ident_rot, ident_vec, camera_matrix, camera_dist)
+                    frame = cv2.putText(frame, str(ident), tuple(projected.ravel()), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                        (0, 0, 255), thickness=2)
+                    projected2, _ = cv2.projectPoints(FIDUCIAL_CORNERS, ident_rot, ident_loc, camera_matrix,
+                                                      camera_dist)
+                    cv2.circle(frame, tuple(projected.ravel()), 5, (0, 0, 255), -1)
+                    frame = cv2.polylines(frame, [np.int32(projected2).reshape(-1, 1, 2)], True, (0, 0, 255), 2)
+
+                    est_ref_loc = ident_loc + ident_matrix.dot(link.translation)
+                    est_ref_rot = ident_matrix.dot(link.rotation)  # link.rotation.T.dot(ident_matrix)
+                    projected3, _ = cv2.projectPoints(FIDUCIAL_CORNERS, est_ref_rot, est_ref_loc, camera_matrix,
+                                                      camera_dist)
+                    frame = cv2.polylines(frame, [np.int32(projected3).reshape(-1, 1, 2)], True, (0, 255, 0), 2)
 
         cv2.imshow("tracker", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
